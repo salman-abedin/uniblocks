60e049d802cadebd50263cf3239c3f085c26979c
Salman Abedin <salmanabedin@zohomail.com>
2020-06-23 21:00:16 +0600

    update

--- uniblocks.sh
+++ uniblocks.sh
@@ -3,7 +3,6 @@
 case $1 in
     --server)
         [ "$PANEL_FIFO" ] || export PANEL_FIFO=/tmp/panelFifo
-        [ "$REC_PID" ] || export REC_PID=/tmp/recPid
         [ "$UNIBLOCKS_PID" ] || export UNIBLOCKS_PID=/tmp/ubPid
         DUMMYFIFO=/tmp/dff
 
@@ -47,7 +46,69 @@ case $1 in
         done
         ;;
     --client)
-        :
+        del="  |  "
+        # status=""
+
+        refresh-block 9
+        sleep 1
+
+        while read -r line; do
+
+            # grep -Ev "^#|^$" ~/.config/uniblocksrc |
+            #     while read -r line; do
+            #         if [ "$(echo "$line" | cut -d, -f3)" = 0 ]; then
+            #             pipe_static \
+            #                 "$(echo "$line" | cut -d, -f1)" \
+            #                 "$(echo "$line" | cut -d, -f2)"
+            #         else
+            #             pipe_dynamic \
+            #                 "$(echo "$line" | cut -d, -f1)" \
+            #                 "$(echo "$line" | cut -d, -f2)" \
+            #                 "$(echo "$line" | cut -d, -f3)"
+            #         fi
+            #     done
+
+            case $line in
+
+                d*) dt="${line#?}" ;;
+                m*) mail="${line#?}" ;;
+                n*) not="${line#?}" ;;
+                r*) rec="${line#?}" ;;
+                s*) sys="${line#?}" ;;
+                v*) vol="${line#?}" ;;
+                w*) wif="${line#?}" ;;
+
+                W*)
+                    wm=
+                    IFS=':'
+                    set -- ${line#?}
+                    while [ "$#" -gt 0 ]; do
+                        item="$1"
+                        name="${item#?}"
+                        case "$item" in
+                            [mMfFoOuULG]*)
+                                case "$item" in
+                                    [FOU]*) name=" 🏚  " ;;
+                                    f*) name=" 🕳  " ;;
+                                    o*) name=" 🌴 " ;;
+                                    LM | G*?) name="" ;;
+                                    *) name="" ;;
+                                esac
+                                wm="${wm} ${name}"
+                                ;;
+                        esac
+                        shift
+                    done
+                    ;;
+
+            esac
+
+            # [ "$1" = wif ] && refresh-block <SIG> && echo "$wif" && exit
+
+            printf "%s\r" \
+                "$wif $del $mail $del $not $del $vol $del $wm $del $sys $del $dt $rec"
+
+        done < "$PANEL_FIFO"
         ;;
     *) : ;;
 esac

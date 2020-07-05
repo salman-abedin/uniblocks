8fac9539af0e8b3d106b0d1e6f9663d200fc78cc
salman-abedin <salman.abedin2@g.bracu.ac.bd>
2020-07-05 17:36:07 +0600

    update

--- uniblocks.sh
+++ uniblocks.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env sh
 
-[ "$PANEL_FIFO" ] || export PANEL_FIFO=/tmp/panelFifo
+[ "$PANELFIFO" ] || export PANELFIFO=/tmp/panelFifo
 
 case $1 in
     --server)
-        [ "$UNIBLOCKS_PID" ] || export UNIBLOCKS_PID=/tmp/ubPid
+        [ "$UBPID" ] || export UBPID=/tmp/ubPid
         DUMMYFIFO=/tmp/dff
         pipeToFifo() {
             if [ "$3" = 0 ]; then
-                echo "$1""$("$2")" > "$PANEL_FIFO" &
+                echo "$1""$("$2")" > "$PANELFIFO" &
             else
                 while :; do
                     echo "$1""$($2)"
                     sleep "$3"
-                done > "$PANEL_FIFO" &
+                done > "$PANELFIFO" &
             fi
         }
         generateBlocks() {
-            [ -e "$PANEL_FIFO" ] && rm "$PANEL_FIFO"
-            mkfifo "$PANEL_FIFO"
+            [ -e "$PANELFIFO" ] && rm "$PANELFIFO"
+            mkfifo "$PANELFIFO"
             grep -Ev "^#|^$" ~/.config/uniblocksrc |
                 while read -r line; do
                     pipeToFifo \
@@ -27,14 +27,14 @@ case $1 in
                         "$(echo "$line" | cut -d, -f3)"
                 done
 
-            bspc subscribe report > "$PANEL_FIFO" &
+            bspc subscribe report > "$PANELFIFO" &
         }
         trap 'canberra-gtk-play -i audio-volume-change && pipeToFifo v "volume" 0' RTMIN+1
         trap 'pipeToFifo m "mailbox" 0' RTMIN+2
         trap 'pipeToFifo n "noti-stat" 0' RTMIN+3
         trap 'pgrep -P $$ | grep -v $$ | xargs kill -9; generateBlocks' RTMIN+9
 
-        echo $$ > "$UNIBLOCKS_PID"
+        echo $$ > "$UBPID"
         [ -e "$DUMMYFIFO" ] && rm -f "$DUMMYFIFO"
         mkfifo "$DUMMYFIFO"
         while :; do
@@ -84,7 +84,7 @@ case $1 in
             # [ "$1" = wif ] && refresh-block <SIG> && echo "$wif" && exit
             printf "%s\r" \
                 "$wif $del $mail $del $not $del $vol $del $wm $del $sys $del $dt $rec"
-        done < "$PANEL_FIFO"
+        done < "$PANELFIFO"
         ;;
     *) : ;;
 esac
